4c96ec2e2cecd489e258cb86ac8bc1dd2bd7d75f
Petros Barreto da silva <>
2024-07-24 15:27:23 -0300

    Adcionando novo teste

--- test/account_test.dart
+++ test/account_test.dart
@@ -11,6 +11,14 @@ void main() {
       expect(account.balance, 0);
     });
 
+    test("Teste de ua nova transferência", () {
+      final Account account = Account(number: 123, cpf: "322.123.123-22", balance: 100.00, name: "Petros")
+    account.transfer(50);
+
+    expect(account.balance, 50);
+
+    });
+
     test("Transferir números negativos", () {
       final Account account = Account(number: 123, cpf: "322.123.123-22", balance: 100.0, name: "Beto");
 
